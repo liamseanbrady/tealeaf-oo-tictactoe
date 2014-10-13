853729b6c08dc4b6c260be5ec206aac0d360a773
Liam Sean Brady <liamseanbrady@gmail.com>
2014-10-13 12:44:48 +0100

    Add some more code to solutions - basic Classes

--- solution_oo_tictactoe.rb
+++ solution_oo_tictactoe.rb
@@ -11,3 +11,53 @@
 # Verbs: start, go, mark, turn, tie, won
 # States: empty, 3 squares in a row
 
+class Board
+
+  def initialize
+    @data = {}
+    (1..9).each { |position| @data[position] = Square.new(" ") }
+  end
+
+  def draw
+    puts "Drawing...board..."
+    puts @data.inspect
+  end
+
+  # - all squares marked?
+  # - find all empty squares
+end
+
+class Square
+  def initialize(value)
+    @value = value
+  end
+end
+
+board = Board.new
+board.draw
+
+# player1
+#   - name
+#   - marker
+
+# squares
+#   - occupied?
+#   - mark(marker)
+
+# game engine
+#   draw a board
+
+#   assign player to "X"
+#   assign computer to "O"
+
+#   loop until a winner or all squares are taken
+#     player picks an empty square
+#     check for a winner
+#     computer picks and empty square
+#     check for a winner
+
+#   if there's a winner
+#     show the winner
+#   or else
+#      it's a tie
+
