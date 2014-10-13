297c2c931e05c096be3b471807f4aa94f8486d05
Liam Sean Brady <liamseanbrady@gmail.com>
2014-10-13 12:05:34 +0100

    Add beginnings of solution code to OO Tic Tac Toe

---
+++ solution_oo_tictactoe.rb
@@ -0,0 +1,13 @@
+# What is a Tic Tac Toe game?
+
+# A board game with two players. We start with an empty 3 x 3 board.
+# One player is X, the other player is O.
+# The 'X' player goes first.
+# Two players alternate to mark empty squares.
+# Alternate turns until a player gets 3 squares in a row.
+# If all squares are marked and nobody won, then it's a tie.
+
+# Nouns: Board, Players, X, O, Squares
+# Verbs: start, go, mark, turn, tie, won
+# States: empty, 3 squares in a row
+
